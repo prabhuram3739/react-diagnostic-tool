8a8065fe1c91cb12907fb0492ae446b6c3a03041
Murugesan <PMurugesan@ad.harman.com>
2020-10-30 16:12:56 +0530

    Added the data enrichment ui modal boxes for the use ccases in the circuit switch

--- src/components/subscriber/simActivateModal/SIMActivateModal.js
+++ src/components/subscriber/simActivateModal/SIMActivateModal.js
@@ -3,7 +3,7 @@ import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 import { makeStyles } from '@material-ui/core/styles';
-import PhoneAndroidOutlinedIcon from '@material-ui/icons/PhoneAndroidOutlined';
+import SimCardOutlinedIcon from '@material-ui/icons/SimCardOutlined';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -71,7 +71,7 @@ function SIMActivateModal(props) {
       
         <React.Fragment>
         <Button className="iconBtn" onClick={handleClickOpen}>
-        <PhoneAndroidOutlinedIcon/>
+        <SimCardOutlinedIcon/>
         </Button>
         <Dialog
           fullWidth={fullWidth}
